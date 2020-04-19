a62144ffce5ed63e385dd4f33456388e1da0acc8
Rafael <rafael.esteller95@gmail.com>
2020-04-19 19:11:56 -0400

    Updates

---
+++ precache-manifest.26e6486fb35226347f77c27e3f683465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb246e7937a245314757df682d2301dd",
+    "url": "/mort/index.html"
+  },
+  {
+    "revision": "05c35206273b88cf3358",
+    "url": "/mort/static/css/main.ccea3b94.chunk.css"
+  },
+  {
+    "revision": "99e2b3f5b789ee845702",
+    "url": "/mort/static/js/2.b26adf55.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/mort/static/js/2.b26adf55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c35206273b88cf3358",
+    "url": "/mort/static/js/main.4f25f969.chunk.js"
+  },
+  {
+    "revision": "4b4925f633fef3b38e42",
+    "url": "/mort/static/js/runtime-main.54bbd163.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mort/static/media/logo.5d5d9eef.svg"
+  }
+]);
